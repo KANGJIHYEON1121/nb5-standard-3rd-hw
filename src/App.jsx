1f9494228813dcf3a9598a60ac3d7f21e8c95ed2
강지현 <kangjihyeon@gangjihyeon-ui-MacBookAir.local>
2024-05-20 18:44:16 +0900

    ex1 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 export default function App() {
   const students = [
-    { name: "Alice", age: 17, grade: "A" },
-    { name: "Bob", age: 18, grade: "B" },
-    { name: "Charlie", age: 16, grade: "C" },
-    { name: "Diana", age: 19, grade: "D" },
+    { name: 'Alice', age: 17, grade: 'A' },
+    { name: 'Bob', age: 18, grade: 'B' },
+    { name: 'Charlie', age: 16, grade: 'C' },
+    { name: 'Diana', age: 19, grade: 'D' },
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((student) => student.age >= 18);
+
+  const handleClick = (student) => {
+    alert(`나이: ${student.age} 
+성적: ${student.grade}`);
+  };
 
   return (
     <div>
@@ -15,6 +20,18 @@ export default function App() {
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+        {filteredStudents.map((student) => {
+          return (
+            <li key={student.name}>
+              <h3
+                onClick={() => handleClick(student)}
+                style={{ cursor: 'pointer' }}
+              >
+                {student.name}
+              </h3>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
